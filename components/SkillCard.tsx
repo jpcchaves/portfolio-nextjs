2cfb041a2ac7416d349c82dfc2b69e8add8485a4
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-04 19:00:52 -0300

    removed console.log from SkillCard component

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -7,7 +7,6 @@ interface SkillCardProps {
 }
 
 const SkillCard = ({ path, skill }: SkillCardProps) => {
-  console.log(path, skill);
   return (
     <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
       <div className="grid grid-cols-2 gap-4 justify-center items-center">
