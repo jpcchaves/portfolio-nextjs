80fa3e1eccc76acb38c9155484efa9b7c15874ad
João Paulo <jpcchaves@outlook.com>
2023-03-06 21:39:44 -0300

    feat: updated skills icons

--- data/skillsListData.ts
+++ data/skillsListData.ts
@@ -1,99 +1,99 @@
-import JS from "../public/assets/skills/javascript.png";
-import TS from "../public/assets/skills/typescript.png"
-import ReactIcon from '../public/assets/skills/react.png'
-import NextIcon from '../public/assets/skills/nextjs.png'
-import TW from '../public/assets/skills/tailwind.png'
-import SC from '../public/assets/skills/styled-components.png'
-import HTMLIcon from "../public/assets/skills/html.png"
-import CSSIcon from "../public/assets/skills/css.png"
-import NodeJsIcon from "../public/assets/skills/node.png"
-import ExIcon from '../public/assets/skills/express.png'
-import MDIcon from '../public/assets/skills/mongo.png'
-import GitIcon from "../public/assets/skills/git.png"
-import GHIcon from "../public/assets/skills/github1.png"
-import Heroku from "../public/assets/skills/heroku.png"
-import Netlify from "../public/assets/skills/netlify.png"
+import TS from "../public/assets/skills/typescript.png";
+import ReactIcon from "../public/assets/skills/react.png";
+import NextIcon from "../public/assets/skills/nextjs.png";
+import TW from "../public/assets/skills/tailwind.png";
+import SC from "../public/assets/skills/styled-components.png";
+import Java from "../public/assets/skills/java.png";
+import SpringBoot from "../public/assets/skills/spring-boot.png";
+import NodeJsIcon from "../public/assets/skills/node.png";
+import ExIcon from "../public/assets/skills/express.png";
+import MySql from "../public/assets/skills/mysql.png";
+import GitIcon from "../public/assets/skills/git.png";
+import GHIcon from "../public/assets/skills/github1.png";
+import Heroku from "../public/assets/skills/heroku.png";
+import Netlify from "../public/assets/skills/netlify.png";
 import PostgreSQL from "../public/assets/skills/postgres.png";
+import Chakra from "../public/assets/skills/chakra.png";
 
 export const SkillsData = [
   {
     id: 1,
-    name: "JavaScript",
-    path: JS
+    name: "Java",
+    path: Java,
+  },
+  {
+    id: 2,
+    name: "Spring Boot",
+    path: SpringBoot,
   },
   {
     id: 2,
     name: "TypeScript",
-    path: TS
+    path: TS,
   },
   {
     id: 3,
     name: "ReactJS",
-    path: ReactIcon
+    path: ReactIcon,
   },
   {
     id: 4,
     name: "NextJS",
-    path: NextIcon
+    path: NextIcon,
   },
   {
     id: 5,
     name: "TailwindCSS",
-    path: TW
+    path: TW,
   },
   {
     id: 6,
     name: "Styled Components",
-    path: SC
+    path: SC,
   },
   {
     id: 7,
-    name: "HTML",
-    path: HTMLIcon
-  },
-  {
-    id: 8,
-   name: "CSS",
-    path: CSSIcon
+    name: "Chakra UI",
+    path: Chakra,
   },
   {
     id: 9,
     name: "NodeJS",
-    path: NodeJsIcon
+    path: NodeJsIcon,
   },
   {
     id: 10,
     name: "Express",
-    path: ExIcon
+    path: ExIcon,
   },
   {
     id: 11,
     name: "PostgreSQL",
-    path: PostgreSQL
+    path: PostgreSQL,
   },
   {
     id: 12,
-    name: "MongoDB",
-    path: MDIcon
+    name: "MySQL",
+    path: MySql,
   },
   {
     id: 13,
     name: "Git",
-    path: GitIcon
+    path: GitIcon,
   },
   {
     id: 14,
     name: "GitHub",
-    path: GHIcon
+    path: GHIcon,
   },
   {
     id: 15,
     name: "Heroku",
-    path: Heroku
+    path: Heroku,
   },
   {
     id: 16,
     name: "Netlify",
-    path: Netlify
-  }
-]
+    path: Netlify,
+  },
+];
