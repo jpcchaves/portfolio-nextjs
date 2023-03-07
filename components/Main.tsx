88048d4b60b238819e8bdfed5399e9f1dfb1c117
João Paulo <jpcchaves@outlook.com>
2023-03-06 21:46:46 -0300

    feat: refactoring main description

--- components/Main.tsx
+++ components/Main.tsx
@@ -15,14 +15,13 @@ const Main = () => {
           <h1 className="py-4 text-gray-700">
             Olá, me chamo <span className="text-[#5651e5]">João</span>
           </h1>
-          <h1 className="py-2 text-gray-700">Sou desenvolvedor Front-End</h1>
+          <h1 className="py-2 text-gray-700">Sou Desenvolvedor de Software</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
             Com dedicação total ao Web Development, estou me especializando na
             construção de sites otimizados e bem estruturados. Atualmente focado
             no desenvolvimento de aplicações web utilizando TypeScript, ReactJS
-            e NextJS enquanto aprendo também tecnologias voltadas para o
-            Back-end (NodeJS, ExpressJS, TypeScript e o banco de dados
-            PostgreSQL).
+            e NextJS no <strong>Front-end</strong> e utilizando Java e Spring
+            Boot no <strong>Back-end</strong>
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
