eeab762cf8fbbefb5d2e69bf64c8e88f71837632
João Paulo <jpcchaves@outlook.com>
2023-04-15 11:13:24 -0300

    feat: add transition effect when hover an project

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -16,7 +16,7 @@ const ProjectItem = ({
   projectStack,
 }: Project) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-[#5651e5] transition duration-300">
       <Image
         className="rounded-xl group-hover:opacity-10"
         src={backgroundImg}
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>João Paulo | Web Developer</title>
+        <title>João Paulo | Software Developer</title>
         <meta name="description" content="Criado utilizando NextJS" />
         <link rel="icon" href="/favicon.io" />
       </Head>
