38c65fc6ae75116c54e8081b1c2e0c3b24851d4f
João Paulo <jpcchaves@outlook.com>
2023-10-03 14:08:38 -0300

    feat: add icons lib css import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import "node_modules/flag-icons/css/flag-icons.min.css";
 
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
