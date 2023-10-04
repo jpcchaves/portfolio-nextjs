0c36a07b4c03051a84d24cd6d9d08f37cf2b7e6b
João Paulo <jpcchaves@outlook.com>
2023-10-03 23:06:18 -0300

    feat: add job role translation in metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,16 @@ import Head from "next/head";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Main from "../components/Main";
-import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
+  const { t } = useTranslation();
   return (
     <div>
       <Head>
-        <title>João Paulo | Software Developer</title>
+        <title>João Paulo | {t("job_role")}</title>
         <meta name="description" content="Criado utilizando NextJS" />
         <link rel="icon" href="/favicon.io" />
       </Head>
