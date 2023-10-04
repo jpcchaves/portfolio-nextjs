1b6b8220795da7eca72d83c749e8f8c7ec621c78
João Paulo <jpcchaves@outlook.com>
2023-10-04 08:58:21 -0300

    fix: adjusted page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,22 @@ import Navbar from "../components/Navbar";
 
 import { appWithI18Next } from "ni18n";
 import { ni18nConfig } from "../ni18n.config";
+import Head from "next/head";
+import { useTranslation } from "react-i18next";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const { t, ready } = useTranslation();
   return (
     <>
+      <Head>
+        {ready ? (
+          <title>João Paulo | {t("job_role")}</title>
+        ) : (
+          <title>João Paulo | Software Engineer</title>
+        )}
+        <meta name="description" content="NextJS APP" />
+        <link rel="icon" href="/favicon.io" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />;
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,12 @@
-import Head from "next/head";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Main from "../components/Main";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-import { useTranslation } from "react-i18next";
 
 export default function Home() {
-  const { t } = useTranslation();
   return (
     <div>
-      <Head>
-        <title>João Paulo | {t("job_role")}</title>
-        <meta name="description" content="Criado utilizando NextJS" />
-        <link rel="icon" href="/favicon.io" />
-      </Head>
       <Main />
       <About />
       <Skills />
