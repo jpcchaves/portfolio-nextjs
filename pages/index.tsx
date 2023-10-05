dc1fd0c958f325ec1886e7bc4963f5b9acc82e67
João Paulo <jpcchaves@outlook.com>
2023-10-04 22:34:49 -0300

    fix: add waiting until i18n loads

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,24 @@ import Contact from "../components/Contact";
 import Main from "../components/Main";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
+  const { ready } = useTranslation();
+
   return (
-    <div>
-      <Main />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
+    <>
+      {ready ? (
+        <div>
+          <Main />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+        </div>
+      ) : (
+        <>Loading...</>
+      )}
+    </>
   );
 }
